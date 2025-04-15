bdc1765d40047ae2abe351faa5b889029725101c
shefket-mustafa <miracle_1@abv.bg>
2025-04-15 15:40:32 +0300

    Home page base styling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,25 @@
+import { NavLink } from 'react-router';
 import './home.css'
+import welcomingImage from '/images/home-page-container.jpg';
 
 export default function Home() {
   return (
+  <>
+  <div className='home-page-container'>
 
-    <p>Home</p>
-      
+    <div className='sub-container'>
+    <div className='home-page-welcoming'>
+    <p>Welcome to <strong>Moviegram</strong> — your gateway to the world of movies. <br/>  
+     Discover trending films, search by title, and explore <br/> cinematic gems from around the globe.</p>
+      <NavLink className={'home-nav'} to={'/movies'}>Explore</NavLink>
+    </div>
+
+    <div className='welcoming-image'>
+      <img src={welcomingImage} alt="Loading.." />
+    </div>
+
+    </div> 
+  </div>
+  </>
     );
 }
