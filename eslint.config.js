ec1382003073861f0a96c20af08ebb9dd0e8a3b5
shefket-mustafa <miracle_1@abv.bg>
2025-04-16 15:42:20 +0300

    I removed the warning for this symbol ''

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,8 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
-      'no-unused-vars':'off'
+      'no-unused-vars':'off',
+      'react/no-unescaped-entities': 'off'
     },
   },
 ]
