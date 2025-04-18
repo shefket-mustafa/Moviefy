04584250417ccc9a5c76f6db476edc6940750eff
shefket-mustafa <miracle_1@abv.bg>
2025-04-18 23:19:38 +0300

    Custom hook for fetching movies created

---
+++ src/api/requests.js
@@ -0,0 +1,45 @@
+// myAPI b4a2347f7dacd3d039a3465b4a2ccc4a
+
+// https://api.themoviedb.org/3/search/movie?query=Gladiator&api_key=YOUR_API_KEY
+// Returns:
+
+// Title
+
+// Overview (plot summary)
+
+// Release date
+
+// Poster path
+
+// Popularity, vote average, etc.
+//SEARCH by title
+
+// https://api.themoviedb.org/3/movie/popular?api_key=YOUR_API_KEY returns 20 popular movies
+
+//https://api.themoviedb.org/3/movie/155?api_key=YOUR_API_KEY search by id 
+
+const baseUrl = 'https://api.themoviedb.org/3/movie/'
+
+const useGetMovies = () => {
+
+    const getPopularMovies = async () => {
+
+        try{
+            const response = await fetch(baseUrl + `popular?api_key=b4a2347f7dacd3d039a3465b4a2ccc4a`);
+
+            if(!response.ok){
+                throw new Error('Failed to fetch popular movies');
+            }
+            const data = await response.json();
+            return data;
+
+        }catch(err){
+            console.error('Error Fetching (catch)')
+            return [];
+        };
+    }
+    return {getPopularMovies}
+};
+
+export {useGetMovies};
+
