d135b2cf1a97ea7dfe6240ddc384d915226d702d
shefket-mustafa <miracle_1@abv.bg>
2025-04-18 23:14:00 +0300

    Changing architecture of week's top 5 movies

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,7 +1,50 @@
+import { Link } from 'react-router';
 import './movies.css'
 
 export default function Movies() {
   return (
-      <h1>Movies</h1>
+    <div className='movies-container'>
+
+     <div className='top-container'>
+
+      
+      <p className='top-text'>🔥 This Week's Most Popular Movies</p>
+     
+
+     <div className='week-5'>
+
+      <div className='top-5'>
+
+        <div className='item'>
+          <div className='item-number'><strong>01</strong>
+          <div className='item-text'>Gladiator</div>
+          </div>
+
+        <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_8Sck6PjokJTJj9f6GDCgOLTzcHa6JYbKTa29Jg4L1oOAgq9j0XbX0E8JEGn877k6MhQRlgWEwah3hzv5SCWFuhs3tb3wI1ml7V01HoKzUDPlKpZp0ys3SYguP6N0mKVmm3KqLwogUA6w/s1600/one_hundred_twenty_seven_hours.jpg" alt="Loading.." />
+        </div>
+
+      </div>
+
+      <div className='top-5'>
+      <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_8Sck6PjokJTJj9f6GDCgOLTzcHa6JYbKTa29Jg4L1oOAgq9j0XbX0E8JEGn877k6MhQRlgWEwah3hzv5SCWFuhs3tb3wI1ml7V01HoKzUDPlKpZp0ys3SYguP6N0mKVmm3KqLwogUA6w/s1600/one_hundred_twenty_seven_hours.jpg" alt="Loading.." />
+      </div>
+
+      <div className='top-5'>
+      <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_8Sck6PjokJTJj9f6GDCgOLTzcHa6JYbKTa29Jg4L1oOAgq9j0XbX0E8JEGn877k6MhQRlgWEwah3hzv5SCWFuhs3tb3wI1ml7V01HoKzUDPlKpZp0ys3SYguP6N0mKVmm3KqLwogUA6w/s1600/one_hundred_twenty_seven_hours.jpg" alt="Loading.." />
+      </div>
+
+      <div className='top-5'>
+      <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_8Sck6PjokJTJj9f6GDCgOLTzcHa6JYbKTa29Jg4L1oOAgq9j0XbX0E8JEGn877k6MhQRlgWEwah3hzv5SCWFuhs3tb3wI1ml7V01HoKzUDPlKpZp0ys3SYguP6N0mKVmm3KqLwogUA6w/s1600/one_hundred_twenty_seven_hours.jpg" alt="Loading.." />
+      </div>
+
+      <div className='top-5'>
+      <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg_8Sck6PjokJTJj9f6GDCgOLTzcHa6JYbKTa29Jg4L1oOAgq9j0XbX0E8JEGn877k6MhQRlgWEwah3hzv5SCWFuhs3tb3wI1ml7V01HoKzUDPlKpZp0ys3SYguP6N0mKVmm3KqLwogUA6w/s1600/one_hundred_twenty_seven_hours.jpg" alt="Loading.." />
+      </div>
+
+     </div>
+
+     </div>
+
+    </div>
     );
 }
