92dbf3662bf9db6a154d37aed4ce2dfa761f759e
shefket-mustafa <miracle_1@abv.bg>
2025-05-02 23:40:37 +0300

    Title removed for better ui

--- src/components/top-5-movies/Top5Movie.jsx
+++ src/components/top-5-movies/Top5Movie.jsx
@@ -1,11 +1,6 @@
 export default function Top5Movie({ movieData,node }) {
   return (
     <div className='top-5'>
-        <div className='item'>
-          
-          <div className='item-text'>{movieData.title}</div>
-          </div>
-
         <img src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`} alt="Loading.." />
         </div>
     );
