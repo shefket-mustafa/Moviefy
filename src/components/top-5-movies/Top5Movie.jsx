1ae9ac48cdfd1a285c2459a0dcffdba5cfa99a3e
shefket-mustafa <miracle_1@abv.bg>
2025-05-04 00:51:12 +0300

    details added to each movies

--- src/components/top-5-movies/Top5Movie.jsx
+++ src/components/top-5-movies/Top5Movie.jsx
@@ -1,7 +1,21 @@
 export default function Top5Movie({ movieData }) {
+  const releaseDate = movieData.release_date;
+  const year = new Date(releaseDate).getFullYear();
+  const vote = Math.floor(movieData.vote_average);
   return (
-    <div className='top-5'>
-        <img src={`https://image.tmdb.org/t/p/w500${movieData.backdrop_path}`} alt="Loading.." />
+    <div className='top-5' 
+    style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData?.backdrop_path})`}}>
+        
+        <div className="details">
+          <p style={{fontSize: '15px'}}>{movieData.title}</p>
+          <p style={{fontSize: '12px'}}>{year}</p>
+        </div>
+
+        <div className="rating">
+        ⭐ {vote} 
+        </div>
+
+
         </div>
     );
 }
