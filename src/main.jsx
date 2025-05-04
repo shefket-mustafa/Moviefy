9a4bb988cf850edd592ec9975854d916fffba9fd
shefket-mustafa <miracle_1@abv.bg>
2025-05-05 00:33:11 +0300

    App wrapped in movie context

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './style.css'
 
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { MovieProvider } from './components/context/MoviesContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
+    <MovieProvider>
     <App />
+    </MovieProvider>
   </StrictMode>
   </BrowserRouter>
 )
