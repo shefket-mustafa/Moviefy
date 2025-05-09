7f48e03570be649901b0614ad25726e36890c4a6
shefket-mustafa <miracle_1@abv.bg>
2025-05-09 18:34:46 +0300

    small changes made

--- src/components/util-hooks/useMovieContext.js
+++ src/components/util-hooks/useMovieContext.js
@@ -1,4 +1,4 @@
 import { useContext } from "react";
-import { MovieContext } from "../context/movieContext";
+import { MovieContext } from "../context/movie-context/movieContext";
 
 export const useMovieContext = () => useContext(MovieContext);
