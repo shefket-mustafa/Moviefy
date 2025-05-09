ba25782ec62c80ed8e662237599d5a95796e5d0b
shefket-mustafa <miracle_1@abv.bg>
2025-05-10 00:15:52 +0300

    Search bar added to the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,33 @@
+
 import { NavLink } from 'react-router';
 import icon from '../../../public/images/header-icon.png'
+import { useMovieContext } from '../util-hooks/useMovieContext'
 import './header.css'
 
 export default function Header() {
+
+  const {setSearchTerm, searchTerm, setIsSearchOpen, searchResults, setSearchResults} = useMovieContext()
+
+  const searchHandler = (term) => {
+    setSearchTerm(term);
+    setIsSearchOpen(true);
+  };
   return (
       <>
       <div className='header'>
 
         <div className='icon'><img src={icon} alt="Loading.." />
             <p className='header-text'>Moviefy</p>
+        </div>
+
+        <div className='search'>
+          <input type="text"
+          name='search' 
+          placeholder='Batman' 
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.currentTarget.value)}
+          />
+          <button onClick={()=>searchHandler(searchTerm)}>Search</button>
         </div>
 
         <div className='nav'>
