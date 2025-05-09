22a41471031095ea68e6387fd070bc715b7efc36
shefket-mustafa <miracle_1@abv.bg>
2025-05-09 18:34:11 +0300

    Handler function added to movie containers which redirects to details component upon press

--- src/components/top-5-movies/Top5Movie.jsx
+++ src/components/top-5-movies/Top5Movie.jsx
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router";
+import { useDetailsContext } from "../util-hooks/useDetailsContext";
+
 export default function Top5Movie({ movieData }) {
+
+  const navigate = useNavigate()
+
   const releaseDate = movieData.release_date;
   const year = new Date(releaseDate).getFullYear();
   const vote = movieData.vote_average;
+
+
+  const clickMovieHandler = (movieData, movieId) => {
+    navigate(`/movies/${movieId}/details`)
+ }
   return (
     <div className='top-5' 
-    style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData?.backdrop_path})`}}>
+    style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData?.backdrop_path})`}}
+    onClick={() => clickMovieHandler(movieData, movieData.id)} 
+    >
         
         <div className="details">
           <p style={{fontSize: '15px'}}>{movieData.title}</p>
